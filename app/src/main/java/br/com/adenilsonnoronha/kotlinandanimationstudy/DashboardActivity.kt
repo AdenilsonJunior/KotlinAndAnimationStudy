53ddffc6f9c274444c8bad49f4f2db12976c84d0
Adenilson Noronha da Silva Junior <noronha.adenilson@gmail.com>
2017-08-28 22:47:15 -0300

    Configure kotlin in project.

---
+++ app/src/main/java/br/com/adenilsonnoronha/kotlinandanimationstudy/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package br.com.adenilsonnoronha.kotlinandanimationstudy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
